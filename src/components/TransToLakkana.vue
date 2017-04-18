b31e745b3a2454a2686ddad8fb1d490b01438a5c
tanaphum <tana_jumbo@hotmail.com>
2017-04-19 01:16:30 +0700

    Update
    Transform Mapping-Table to Lakkana Chart Page

---
+++ src/components/TransToLakkana.vue
@@ -0,0 +1,335 @@
+<template>
+<div class="wrapper">
+<section class="header">
+    <div>
+        <h1 class="header_text"> Transform Mapping-Table to Lakkana Chart </h1>
+    </div>
+    <div class="header_image">
+        <img src="../assets/chart.png" alt="chart">
+    </div>
+</section>
+    <div class = "select_area">
+        <p class="symbol"> 
+        <img src ="../assets/A1.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        <p class="symbol"> 
+         <img src ="../assets/A2.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A3.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A4.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A5.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A6.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A7.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A8.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/A9.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 0 :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+        </p>
+        <p class="symbol"> 
+        <img src ="../assets/ARlux.png"> :
+            <span class="select">
+            <select>
+                <option>none</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+                <option>11</option>
+                <option>12</option>        
+            </select>
+        </span>
+        </p>
+    </div>
+    <div class="transform">
+        <button type="button" class="transform">Transform</button>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+
+};
+
+</script>
+
+<style scoped>
+body {
+    background-color: #333;
+}
+
+p.symbol {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    text-align: center;
+    font-size : 200%;
+    color: black;
+}
+
+button.transform {
+    padding: 10px 0px 10px 0px;
+    border: 3px solid #891abc;
+    width: 35%;
+    color: #891abc;
+    background-color: #fff;
+    font-size: 150%;
+}
+
+div.transform {
+    text-align: center;
+    padding-top: 20px;
+    padding-bottom: 20px;
+
+}
+
+div.header_image {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    text-align: center;
+}
+section.header{
+    background-image: url('../assets/sec1.jpg');
+    background-size: cover;
+    background-position: right;
+    box-shadow: inset 0 0 0 1000px rgba(0,0,0,0.6);
+    padding-top: 20px;
+}
+span.select {
+    border: 1px solid #891abc;
+}
+
+table.mapping {
+    border: 1px solid #891abc;
+    background-color: #fff;
+    width: 40%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+}
+
+td.mapping-table {
+    text-align: center;
+    border: 1px solid #891abc;
+    padding: 10px 0px;
+    color: #891abc;
+}
+
+th.mapping-table {
+    text-align: center;
+    border: 1px solid #891abc;
+    color: #891abc;
+}
+
+div.mapping-table {
+    text-align: center;
+    padding: 20px 0px;
+}
+h1.header_text{
+    text-align: center;
+    font-size : 150%;
+    color: #bbb;
+}
+div.select_area{
+     background-color: #891abc;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '@/components/Index';
 import Exercise from '@/components/Exercise';
+import TranstoLukkana from '@/components/TransToLakkana';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
             name: 'exercise',
             path: '/exercise',
             component: Exercise,
+        },
+        {
+            name: 'TranstoLukkana',
+            path: '/TranstoLukkana',
+            component: TranstoLukkana,
         },
     ],
 });
